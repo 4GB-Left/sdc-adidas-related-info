8eafa852fb7702d1bdd70304fc70b6d184a883ec
Charlie Thao <cthao309@gmail.com>
2020-06-03 13:20:59 -0700

    saved

--- server/sdc-server.js
+++ server/sdc-server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // define express middleware
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://52.53.187.183:5000/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
